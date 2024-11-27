32279875ea9f363becd2aae24c26f2bd8185b42c
RJMakarenko <propusk2@mail.ru>
2024-11-27 17:06:56 +0300

    Begin lesson_2

---
+++ lesson_2.py
@@ -0,0 +1,10 @@
+def function():
+    print('Hello world!')
+
+def main():
+    print('Второй урок Git.')
+    print('Сегодня мы научимся писать:')
+    function()
+
+if __name__ == '__main__':
+    main()
